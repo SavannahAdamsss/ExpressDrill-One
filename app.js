9068b166b60f14e07b23dc3c9f5ee438c7dd4c2f
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-24 10:43:57 -0600

    Create new Repo for kicked-back drills

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./cohorts");
+const app = express();
+let PORT = process.env.PORT || 3000;
+
+function findById(data, id){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id == id){
+            return data[i];
+        }
+    }
+    return null;
+}
+
+app.use(cors());
+
+app.get("/", function (request, response) {
+    response.json({data});
+});
+
+app.get("/:id", function (request, response) {
+    var record = findById(data, request.params.id);
+    if (!record){
+        response.status(404).json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    } else {
+        response.json({data: record});
+    }
+});
+
+app.listen(PORT);
